3ac681d7c31c79d10220cf8651084040ede6427e
Kurt Garloff <kurt@garloff.de>
2024-04-04 18:20:24 +0200

    ssh-keygen for key generation, detect public, AZ.
    * Since nova microversion 2.92, nova does not generate keypairs any
      more. Passing in public_key is mandatory and heat uses the latest
      and greatest API. So use ssh-keygen instead.
    * If not overriden with public: setting, detect external network
    * If not overriden with availability_zone: setting, detect AZ
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- cleanup-jitsi.sh
+++ cleanup-jitsi.sh
@@ -5,13 +5,14 @@
 # STACKNAME defaults to the only deployed stack by default
 # (c) Kurt Garloff <scs@garloff.de>, 2/2020, CC-BY-SA 3.0
 #cd ~
-source .ostackrc.JITSI
+if test -r "$1"; then USERNM="${1%.yml}"; USERNM="${USERNM##*-user-}"; else USERNM="$1"; fi
+if test -z "$OS_CLOUD" -a -r ",ostackrc.$USERNM"; then source .ostackrc.$USERNM; fi
+if test -z "$OS_CLOUD" -a -r ",ostackrc.JITSI"; then source .ostackrc.JITSI; fi
 STACK=$(openstack stack list -f value -c "Stack Name" -c "Stack Status" | grep jitsi)
 STACK_NM=${STACK%% *}
 if test -z "$STACK_NM"; then echo "Could not find jitsi stack to delete."; exit 1; fi
 if test -n "$1"; then
   # User might have passed not just USERNM but template filename
-  if test -r "$1"; then USERNM="${1%.yml}"; USERNM="${USERNM##*-user-}"; else USERNM="$1"; fi
   if test "$STACK_NM" != "jitsi-$USERNM"; then
     echo "WARNING: Found $STACK_NM, but requesting deletion of jitsi-$USERNM"
     STACK_NM="jitsi-$USERNM"
@@ -22,8 +23,9 @@ fi
 echo "Cleaning stack $STACK_NM"
 if ! [[ "$STACK" == *"$STACK_NM"* ]]; then echo "No such stack $STACK_NM"; exit 2; fi
 JITSI_ADDRESS=$(openstack stack output show ${STACK_NM} jitsi_address -f value -c output_value)
-ssh -o StrictHostKeyChecking=no -i jitsi-$USERNM.ssh linux@$JITSI_ADDRESS sudo /root/down.sh
+ssh -o StrictHostKeyChecking=no -i jitsi-$USERNM linux@$JITSI_ADDRESS sudo /root/down.sh
 openstack stack delete -y --wait $STACK_NM
+rm -f $STACK_NM $STACK_NM.pub
 STACK=$(openstack stack list -f value -c "Stack Name" -c "Stack Status")
 if ! [[ "$STACK" == *"$STACK_NM"* ]]; then exit 0; fi
 openstack server list
